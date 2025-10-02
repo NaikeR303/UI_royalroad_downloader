e433ab39444ab4911d481c9050c854bc4c6ef232
NaikeR303 <moikotvaska2004@gmail.com>
2025-10-02 23:08:38 +0300

    Removed cache

--- downloader.py
+++ downloader.py
@@ -3,7 +3,6 @@ import re, requests, bs4, os
 
 class Downloader:
     list = None
-    cache_folder = "cache"
 
     ficton_id = None
 
@@ -21,16 +20,6 @@ class Downloader:
         #Full Url
         self.url = "https://www.royalroad.com/fiction/" + self.ficton_id
 
-        #Creatin' folders
-        if not os.path.exists(self.cache_folder):
-            os.mkdir(self.cache_folder)
-
-        self.folder_path = self.cache_folder + "/" + self.ficton_id
-        if not os.path.exists(self.folder_path):
-            os.mkdir(self.folder_path)
-
-        os.chdir(self.folder_path)
-
     def get_url_list(self):
         soup = bs4.BeautifulSoup(requests.get(url=self.url).content, features="html.parser")
 
@@ -44,6 +33,8 @@ class Downloader:
             chapter["date"] = table_chap[1].find("time")["datetime"].split("T")[0]
             chapter["url"] = "https://www.royalroad.com" + table_chap[1]["href"]
 
+            chapter["content"] = None
+
             list.append(chapter)
 
         self.list = list
@@ -54,12 +45,8 @@ class Downloader:
             self.get_url_list()
         
         for chapter in self.list:
-            chapter_id = chapter["url"].split("/")[7]
-
-            if not os.path.exists(chapter_id):
-                os.mkdir(chapter_id)
-                soup = bs4.BeautifulSoup(requests.get(self.url + "/" + chapter_id))
+            soup = bs4.BeautifulSoup(requests.get(chapter["url"]))
 
 if __name__ == "__main__":
     g = Downloader("https://www.royalroad.com/fiction/33844/the-runesmith")
-    g.download()
+    print(g.get_url_list())
